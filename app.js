bacbf89f0641f66edd07bbe26482a749eb2b9bad
Richard Creeger <richard.creeger@gmail.com>
2018-08-27 01:17:35 -0700

    couple extra styling changes

--- app.js
+++ app.js
@@ -12,16 +12,16 @@ fetch('https://lit-fortress-6467.herokuapp.com/object')
   .then(function(data) {
     return data.json()
   }).then(function(results) {
-    let randomAlbum1 = getRandom(0,4)
-    let randomAlbum2 = getRandom(0,4)
-    let randomAlbum3 = getRandom(0,4)
+    let randomAlbum1 = getRandom(0,5)
+    let randomAlbum2 = getRandom(0,5)
+    let randomAlbum3 = getRandom(0,5)
 
     firstAlbum.setAttribute('src', `./images/${results.results[randomAlbum1].cover_art}`)
 
     if (randomAlbum2 !== randomAlbum1) {
       secondAlbum.setAttribute('src', `./images/${results.results[randomAlbum2].cover_art}`)
     }else {
-      for(let i=0; i<5; i++){
+      for(let i=0; i<1; i++){
         if(randomAlbum1 !== i){
           randomAlbum2 = i;
           secondAlbum.setAttribute('src', `./images/${results.results[randomAlbum2].cover_art}`)
@@ -31,7 +31,7 @@ fetch('https://lit-fortress-6467.herokuapp.com/object')
     if(randomAlbum3 !== randomAlbum2 && randomAlbum3 !== randomAlbum1) {
       thirdAlbum.setAttribute('src', `./images/${results.results[randomAlbum3].cover_art}`)
     }else {
-      for(let j=0; j<5; j++){
+      for(let j=0; j<1; j++){
         if(randomAlbum1 !== j && randomAlbum2 !== j){
           randomAlbum3 = j
           thirdlbum.setAttribute('src', `./images/${results.results[randomAlbum3].cover_art}`)
